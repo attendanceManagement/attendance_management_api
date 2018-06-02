9adccd2b918563ffac8466ef0083e644f0f9504c
attendanceManagement <attendancemanagement32@gmail.com>
2018-06-02 12:42:40 +0530

    export feature

--- services/AppManagerAPI/app/api/attendance.js
+++ services/AppManagerAPI/app/api/attendance.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 const api = {};
+var DateOnly = require('dateonly');
 
 api.getAll = (User, Attendance, Token) => (req, res) => {
   if (Token) {
@@ -31,6 +32,7 @@ api.getByEmp = (User, Attendance, Token) => (req, res) => {
         $lte: end
       }
     }
+    console.log(query);
     Attendance.find({
       emp_id: req.query.emp_id,
       date: query
@@ -44,6 +46,64 @@ api.getByEmp = (User, Attendance, Token) => (req, res) => {
     message: 'Unauthorized'
   });
 }
+
+
+
+api.getAllEmp = (User, Attendance, Token) => (req, res) => {
+  if (Token) {
+    let query = {};
+    query = {
+      $lt: Date.now()
+    }
+
+    if (req.query.month) {
+     
+      const start = new Date(req.query.month);
+      const end = new Date(req.query.month);
+       console.log(new DateOnly(start).valueOf())
+      end.setMonth(end.getMonth() + 1);
+      query = {
+        $gte: new DateOnly(start).valueOf(),
+        $lte: new DateOnly(end).valueOf()
+      }
+    }
+    console.log(query)
+    Attendance.aggregate([
+      {$match:{
+           date: query
+    }},   
+      {
+        $group: {
+          _id: "$emp_id",
+          "attendance": {
+            $push: "$$ROOT"
+          }
+        }
+      },
+       {$lookup: {
+        from: "users",
+        localField: "_id",
+        foreignField: "emp_id",
+        as: "user"
+      }
+    }, {
+      $unwind: "$user"
+    },
+    ]).exec((error, Attendance) => {
+
+if (error) return res.status(400).json(error);
+      res.status(200).json(Attendance);
+      return true;
+
+
+    })
+  } else return res.status(403).send({
+    success: false,
+    message: 'Unauthorized'
+  });
+}
+
+
 
 api.checkAttendance = (Attendance, Token) => (req, res) => {
   if (Token) {
@@ -65,7 +125,7 @@ api.checkAttendance = (Attendance, Token) => (req, res) => {
 api.store = (Regularize, Attendance, Token) => (req, res) => {
   if (Token) {
     const date = req.body.date ? req.body.date : Date.now();
-    let  regId = req.body.regularize_id;
+    let regId = req.body.regularize_id;
     Attendance.find({
       date: date,
       emp_id: req.body.emp_id
@@ -78,7 +138,7 @@ api.store = (Regularize, Attendance, Token) => (req, res) => {
           date: req.body.date,
           in_time: req.body.in_time,
           out_time: req.body.out_time,
-          total_hour : total_hour
+          total_hour: total_hour
         });
 
         attendance.save((error, attendance) => {
@@ -96,15 +156,14 @@ api.store = (Regularize, Attendance, Token) => (req, res) => {
                 regularize: regularize
               });
             });
+          } else {
+            if (error) return res.status(400).json(error);
+            res.status(200).json({
+              success: true,
+              attendance: attendance
+            });
           }
-         else{
-          if (error) return res.status(400).json(error);
-          res.status(200).json({
-            success: true,
-            attendance: attendance
-          });
-         }
-          
+
         })
 
       } else {
@@ -143,15 +202,21 @@ api.checkout = (User, Attendance, Token) => (req, res) => {
     Attendance.findOne({
       emp_id: emp_id,
       _id: attendance_id
-    },(error, attendance) => {
-      if (error) res.status(400).json(error);   
-        attendance.out_time =  Date.now()
-        attendance.total_hour = ((moment(Date.now()).diff(moment(attendance.in_time), 'minutes')) / 60).toFixed(2);
+    }, (error, attendance) => {
+      if (error) res.status(400).json(error);
+      attendance.out_time = Date.now()
+      attendance.total_hour = ((moment(Date.now()).diff(moment(attendance.in_time), 'minutes')) / 60).toFixed(2);
 
-        attendance.save((error,attendance1) => {     
-          if (error) return res.status(400).json({ success: false, message: 'Username or Empoyle ID already exists.' });
-          res.json({ success: true, message: attendance1 });
+      attendance.save((error, attendance1) => {
+        if (error) return res.status(400).json({
+          success: false,
+          message: 'Username or Empoyle ID already exists.'
         });
+        res.json({
+          success: true,
+          message: attendance1
+        });
+      });
     })
   } else return res.status(403).send({
     success: false,
